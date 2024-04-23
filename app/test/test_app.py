47faf058140bfed469ac8c52aa33fbdca156b6d2
MatthewBilo <91922584+MatthewBilo@users.noreply.github.com>
2024-04-23 17:13:08 -0400

    unittests

--- app/test/test_app.py
+++ app/test/test_app.py
@@ -12,43 +12,67 @@ def setup_function():
 def teardown_function():
     db.drop_tables([User, EncryptedData])
 
-def test_register_user():
+def test_register_user_true():
     assert register_user('testuser', 'testpass', 'testkey') == True
-    assert register_user('testuser', 'testpass', 'testkey') == False
 
-def test_check_credentials():
+def test_register_user_false():
+    register_user('testuser', 'testpass', 'testkey')
+    assert register_user('testuser', 'testpass', 'testkey') == False   
+
+def test_check_credentials_true():
     register_user('testuser', 'testpass', 'testkey')
     assert check_credentials('testuser', 'testpass') == (True, None)
     assert check_credentials('wronguser', 'testpass') == (False, "Wrong username")
 
-def test_update_password():
+def test_check_credentials_false():
+    register_user('testuser', 'testpass', 'testkey')
+    assert check_credentials('wronguser', 'testpass') == (False, "Wrong username")
+
+def test_update_password_true():
     register_user('testuser', 'testpass', 'testkey')
     update_password('testuser', 'newpass')
     assert check_credentials('testuser', 'newpass') == (True, None)
+
+def test_update_password_false():
+    register_user('testuser', 'testpass', 'testkey')
     update_password('wronguser', 'newpass')
     assert check_credentials('wronguser', 'newpass') == (False, "Wrong username")
 
-def test_clear_user():
+def test_clear_user_true():
     register_user('testuser', 'testpass', 'testkey')
     clear_user('testuser')
     assert check_credentials('testuser', 'testpass') == (False, "Wrong username")
-    clear_user('wronguser')
-    assert check_credentials('wronguser', 'testpass') == (False, "Wrong username")
 
-def test_save_encrypted_data():
+def test_clear_user_false():
+    register_user('testuser', 'testpass', 'testkey')
+    clear_user('wronguser')
+    assert check_credentials('testuser', 'testpass') == (True, None)
+
+def test_save_encrypted_data_true():
     register_user('testuser', 'testpass', 'testkey')
     save_encrypted_data('testuser', 'testtag', 'testtext')
     assert len(get_encrypted_data('testuser')) == 1
     save_encrypted_data('wronguser', 'testtag', 'testtext')
     assert len(get_encrypted_data('wronguser')) == 0
 
-def test_get_encrypted_data():
+def test_save_encrypted_dat_false():
+    register_user('testuser', 'testpass', 'testkey')
+    save_encrypted_data('testuser', 'testtag', 'testtext')
+    save_encrypted_data('wronguser', 'testtag', 'testtext')
+    assert len(get_encrypted_data('wronguser')) == 0
+
+def test_get_encrypted_data_true():
     register_user('testuser', 'testpass', 'testkey')
     save_encrypted_data('testuser', 'testtag', 'testtext')
     assert len(get_encrypted_data('testuser')) == 1
     assert len(get_encrypted_data('wronguser')) == 0
 
-def test_delete_encrypted_data():
+def test_get_encrypted_data_false():
+    register_user('testuser', 'testpass', 'testkey')
+    save_encrypted_data('testuser', 'testtag', 'testtext')
+    assert len(get_encrypted_data('wronguser')) == 0
+
+def test_delete_encrypted_data_true():
     register_user('testuser', 'testpass', 'testkey')
     save_encrypted_data('testuser', 'testtag', 'testtext')
     encrypted_data = get_encrypted_data('testuser')
@@ -57,4 +81,10 @@ def test_delete_encrypted_data():
     delete_encrypted_data('wronguser', 1)
     assert len(get_encrypted_data('wronguser')) == 0
 
+def test_delete_encrypted_data_false():
+    register_user('testuser', 'testpass', 'testkey')
+    save_encrypted_data('testuser', 'testtag', 'testtext')
+    delete_encrypted_data('wronguser', 1)
+    assert len(get_encrypted_data('wronguser')) == 0
+    assert len(get_encrypted_data('testuser')) == 1
 
